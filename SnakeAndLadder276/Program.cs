e31f465d7b69fb368dc70b1f4a4b73fd04d02922
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-12 23:36:58 +0530

    [Sachin] Adding Welcoe message

---
+++ SnakeAndLadder276/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakeAndLadder276
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder Program");
+        }
+    }
+}
