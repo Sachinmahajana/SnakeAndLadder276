3bfb5b1aa9d2153cbdeca75c883d7f7756a5968d
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-12 23:58:47 +0530

    [Sachin] Adding UC2-Diceroll

--- SnakeAndLadder276/Program.cs
+++ SnakeAndLadder276/Program.cs
@@ -8,6 +8,13 @@
             //UC1-Playerposition
             int start = 0;
             Console.WriteLine("Start Position:" + start);
+
+            //UC2-Diceroll
+            Random random = new Random();
+            int roll = random.Next(0, 7);
+            start = start + roll;
+            Console.WriteLine("You Got : " + roll);
+            Console.WriteLine("Player position at : " + start);
         }
     }
 }
