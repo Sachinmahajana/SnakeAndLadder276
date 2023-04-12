e61c6587586a793fe2c53bc5285d58182c63eb43
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-13 00:28:02 +0530

    [Sachin] Adding UC4-Exactwinigposition

--- SnakeAndLadder276/Program.cs
+++ SnakeAndLadder276/Program.cs
@@ -9,29 +9,32 @@
             int PlayerPosition = 0;
             int start = 0;
             Console.WriteLine("Start Position:" + start);
-
-            //UC2-Diceroll
-            Random random = new Random();
-            int roll = random.Next(0, 7);
-            start = start + roll;
-            Console.WriteLine("You Got : " + roll);
-            Console.WriteLine("Player position at : " + start);
-            //UC3-Checkplayer Position
-            int Option = random.Next(0, 3);
-            Console.ReadLine();
-            if (Option == 0)
+            while (PlayerPosition < 100)
             {
-                PlayerPosition = PlayerPosition - roll;
-                Console.WriteLine("snake " + PlayerPosition);
-            }
-            else if (Option == 1)
-            {
-                PlayerPosition = PlayerPosition + roll;
-                Console.WriteLine("ladder " + PlayerPosition);
-            }
-            else
-            {
-                Console.WriteLine("no play " + PlayerPosition);
+                //UC2-Diceroll
+                Random random = new Random();
+                int roll = random.Next(0, 7);
+                start = start + roll;
+                Console.WriteLine("You Got : " + roll);
+                Console.WriteLine("Player position at : " + start);
+                //UC3-Checkplayer Position
+                int Option = random.Next(0, 3);
+                Console.WriteLine("Option is " + Option);
+                Console.ReadLine();
+                if (Option == 0)
+                {
+                    PlayerPosition = PlayerPosition - roll;
+                    Console.WriteLine("snake " + PlayerPosition);
+                }
+                else if (Option == 1)
+                {
+                    PlayerPosition = PlayerPosition + roll;
+                    Console.WriteLine("ladder " + PlayerPosition);
+                }
+                else
+                {
+                    Console.WriteLine("no play " + PlayerPosition);
+                }
             }
         }
     }
