5fa6fc36bf1dc6c1db49807e61a042f4af3467ba
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-12 23:44:13 +0530

    [Sachin] Adding UC1-Playeposition

--- SnakeAndLadder276/Program.cs
+++ SnakeAndLadder276/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Snake and Ladder Program");
+            //UC1-Playerposition
+            int start = 0;
+            Console.WriteLine("Start Position:" + start);
         }
     }
 }
